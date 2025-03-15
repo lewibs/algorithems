772a815aea960b049bbfb23b05af4a0728fad569
lewibs <benjaminsl2000@gmail.com>
2025-03-15 13:04:43 -0400

    update

--- str_to_int/str_to_int.py
+++ str_to_int/str_to_int.py
@@ -2,23 +2,10 @@ import unittest
 
 # convert numbers like 013, 123, 85647 to ints
 def str_to_int(s):
-    if s[0] == "-":
-        s = s[1:]
-        mod = -1
-    elif s == "0":
-        return 0
-    elif s[0] == "0":
-        return False
-    else:
-        mod = 1
-
     number = 0
     for c in s:
-        if c.isnumeric():
-            number = number * 10 + int(c)
-        else:
-            return False
-    return number * mod
+        number = number * 10 + int(c)
+    return number
 
 class UnitTest(unittest.TestCase):
     def setUp(self):
@@ -27,11 +14,9 @@ class UnitTest(unittest.TestCase):
     def test(self):
         tests = {
             "123":123,
-            "0":0,
-            "01":False,
-            "-5":-5,
-            "5.5":False,
-            "sdf":False,
+            "5644": 5644,
+            "19": 19,
+            "0": 0,
         }
 
         for input, output in tests.items():
